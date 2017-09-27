8791cb7842d8c20beba222960c537ff24f6ededd
jackhill5313 <32319143+jackhill5313@users.noreply.github.com>
2017-09-26 23:54:14 -0400

    Create hetaliabot.js

---
+++ hetaliabot.js
@@ -0,0 +1,58 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("dong")) {
+    message.channel.send("long :eyes:");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("fight me bitch")) {
+    message.channel.send(":muscle::eyes::muscle:");
+  }
+});
+
+client.on("message", (message) => {
+if (message.author.id === '258431184785113094') {
+    message.channel.send("I LOVE YOU SO MUCH AL AND I BELIEVE IN YOU");
+  }
+});
+
+
+
+client.on("message", (message) => {
+if (message.author.id === '258431184785113094' && message.content.includes ('gross')) {
+    message.channel.send("I LOVE YOU SO MUCH AL AND I BELIEVE IN YOU!!!");
+  }
+});
+
+client.on("message", (message) => {
+if (message.author.id === '258431184785113094' && message.content.includes ('disgusting')) {
+    message.channel.send("I LOVE YOU SO MUCH AL AND I BELIEVE IN YOU!!!");
+  }
+});
+
+client.on("message", (message) => {
+if (message.author.id === '258431184785113094' && message.content.includes ('sad')) {
+    message.channel.send("I LOVE YOU SO MUCH AL AND I BELIEVE IN YOU!!!");
+  }
+});
+
+client.on("message", (message) => {
+if (message.author.id === '258431184785113094' && message.content.includes ('cant')) {
+    message.channel.send("I LOVE YOU SO MUCH AL AND I BELIEVE IN YOU!!!");
+  }
+});
+
+client.login("MzYyMjk4MTcyNzU3NzcwMjUz.DKwoDw.Ta-39UJ9Cyr-bZ5GjrtOCP4VvPI");
