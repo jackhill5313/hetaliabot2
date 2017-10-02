269c64046d9d05a230acd711da62314d2ec49844
jackhill5313 <32319143+jackhill5313@users.noreply.github.com>
2017-10-02 13:57:05 -0400

    Update hetaliabot.js

--- hetaliabot.js
+++ hetaliabot.js
@@ -202,8 +202,16 @@ client.on('message', message =>{
 	if (message.content.includes ('rock paper scissors')) {
 		var choices = ['rock','paper','scissors'];
 		var response = choices[Math.floor(Math.random()*3)];
-		message.channel.send('I chose '+response);
-			}
+		.then(() => {
+           message.channel.awaitMessages(response => response.content === 'rock', {
+          max: 1,
+          time: 10000,
+          errors: ['time'],
+        })
+        .then(() => {
+          message.channel.send('I chose '+response);
+	}
 });
+
 
 client.login("MzYyMjk4MTcyNzU3NzcwMjUz.DKwoDw.Ta-39UJ9Cyr-bZ5GjrtOCP4VvPI");
