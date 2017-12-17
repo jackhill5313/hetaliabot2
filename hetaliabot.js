5d59f0f13de53934d517d5f8a9bc5427da4b230f
jackhill5313 <32319143+jackhill5313@users.noreply.github.com>
2017-12-17 02:50:26 -0500

    Update hetaliabot.js

--- hetaliabot.js
+++ hetaliabot.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json");
+
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -237,4 +239,4 @@ client.on('message', message =>{
 	 
 
 
-client.login("FUCKYOUNAZIS");
+client.login("config.fuckyounazis");
