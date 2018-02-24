626bb7d1bb1b4be937e0263c92e848ab2101e546
jackhill5313 <32319143+jackhill5313@users.noreply.github.com>
2018-02-24 16:26:24 -0500

    Update hetaliabot.js

--- hetaliabot.js
+++ hetaliabot.js
@@ -85,7 +85,7 @@ client.on("message", (message) => {
 
 client.on("message", (message) => {
 if (message.content.includes ('hewwo')) {
-    message.channel.send(":hell:");
+    message.react("417069737801940993");
   }
 });
 
