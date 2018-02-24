1c833a57c62fcdf9fb9354c26b43cfe9d67f02ce
jackhill5313 <32319143+jackhill5313@users.noreply.github.com>
2018-02-24 16:56:10 -0500

    Update hetaliabot.js

--- hetaliabot.js
+++ hetaliabot.js
@@ -85,7 +85,7 @@ client.on("message", (message) => {
 
 client.on("message", (message) => {
 if (message.content.includes ('hewwo')) {
-    message.react("413948959908823041");
+    message.react("413948959908823041", "🇵", "🇪", "🇷", "🇮", "🇸", "🇭");
   }
 });
 
