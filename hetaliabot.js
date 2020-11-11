4abfff34dc925eed939f7b797a72fb12fac9702b
jackhill5313 <32319143+jackhill5313@users.noreply.github.com>
2020-11-11 11:55:30 -0500

    Update hetaliabot.js

--- hetaliabot.js
+++ hetaliabot.js
@@ -10,23 +10,33 @@ const ROLE_ID = '265569526781968384';
 const CHANNEL_ID = '265571754393468928';
 
 client.on('guildMemberUpdate', async (oldMember, newMember) => {
+    console.log('Event executed');
     const member = await newMember.fetch();
-    
+
+    console.log('Member Fetched | ', oldMember.roles.cache.has(ROLE_ID), member.roles.caches.has(ROLE_ID));
+
     if (oldMember.roles.cache.has(ROLE_ID) || !member.roles.cache.has(ROLE_ID))
         return;
-        
+
+    console.log('Everything checks out');
+
     const channel = guild.channels.cache.get(CHANNEL_ID) || await guild.channels.fetch(CHANNEL_ID);
-    
+
+    console.log('Channel fetched');
+
     const welcomeEmbed = new Discord.MessageEmbed()
         .setColor('#7ba3ff')
         .setTitle('Welcome!')
         .setDescription(`${newMember} just joined the discord! Make sure to fill out an #intro and get some #roles whenever!`)
         .setFooter('PM PierreBot with any questions!')
         .setTimestamp();
-        
+
     channel.send(welcomeEmbed);
+
+    console.log('Sent');
 });
 
+// THIS IS ALL PING PONG STUFF DOWN HERE
 
   client.on('message', async (message) => {
     if(message.content.toLowerCase().includes ('hewwo')){
@@ -44,168 +54,126 @@ client.on('guildMemberUpdate', async (oldMember, newMember) => {
 };
 });
 
-client.on("message", (message) => {
-if (message.content.includes ('good morning')) {
-    message.channel.send("https://pbs.twimg.com/media/CvIUqUPUAAAGKEg.jpg");
-  }
-});
- 
 client.on("guildMemberAdd", (member) => {
   console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
-  member.guild.channels.get('name', 'rules_and_entry').sendMessage(`Welcome,"${member.user.username}"! Read the rules to enter! 💖`);
+  member.guild.channels.get('name', 'entry').sendMessage(`Welcome,"${member.user.username}"! Read the rules to enter! 💖`);
 });
 
 client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
     message.channel.send("pong!");
   }else if(message.content.startsWith("florida")){
-   message.channel.send(":crocodile: :palm_tree:"); 
+   message.channel.send(":crocodile: :palm_tree:");
   }
 });
 
+//CHAT REACTIONS
 
 client.on("message", (message) => {
 if (message.content.includes ('whos a good boi')) {
     message.channel.send("me!! :heart:");
   }
-});
 
+if (message.content.includes ('good morning')) {
+      message.channel.send("https://pbs.twimg.com/media/CvIUqUPUAAAGKEg.jpg");
+    }
 
-
-client.on('message', message =>{
-	if (message.content.toLowerCase().includes ('romano bot') || message.content.toLowerCase().includes ('romanobot') || message.content.toLowerCase().includes ('romabot') || message.content.toLowerCase().includes ('roma bot')) {
-		var choices = ['yeah.. i owe him .3 BTC but i spent .1 BTC on mcdonalds last week so im a bit short... :thinking:','I sent him an IOU but I think he just deleted it..','I tried to pay him back in coupons to olive garden and he sent me malware','I know him!','i told him about pineapple pizza once... he gave me a glitch after that','Pizza, pasta, put it in a box-'];
-		var response = Math.floor(Math.random() * choices.length);
-    message.channel.send(choices[response]);
-	}
-});
-
-client.on("message", (message) => {
 if (message.content.includes ('good night')) {
-    message.channel.send("http://s2.storage.akamai.coub.com/get/b19/p/coub/simple/cw_timeline_pic/30ccab81aae/01bf7c4bc51b8db5adcde/ios_large_1475540748_image.jpg");
-  }
-});
+        message.channel.send("http://s2.storage.akamai.coub.com/get/b19/p/coub/simple/cw_timeline_pic/30ccab81aae/01bf7c4bc51b8db5adcde/ios_large_1475540748_image.jpg");
+      }
 
-client.on("message", (message) => {
 if (message.content.toLowerCase().includes ('alfred, whats one of the most terrifying things youve seen here on the internet?')) {
-    message.channel.send("https://cdn.discordapp.com/attachments/265571232185843723/462644008020213760/unknown.png");
-  }
-});
+          message.channel.send("https://cdn.discordapp.com/attachments/265571232185843723/462644008020213760/unknown.png");
+        }
 
-
-client.on("message", (message) => {
 if (message.content.includes ('goodnight')) {
-    message.channel.send("http://s2.storage.akamai.coub.com/get/b19/p/coub/simple/cw_timeline_pic/30ccab81aae/01bf7c4bc51b8db5adcde/ios_large_1475540748_image.jpg");
-  }
-});
+            message.channel.send("http://s2.storage.akamai.coub.com/get/b19/p/coub/simple/cw_timeline_pic/30ccab81aae/01bf7c4bc51b8db5adcde/ios_large_1475540748_image.jpg");
+          }
 
-client.on("message", (message) => {
-  if (message.content.startsWith("fight me bitch")) {
-    message.channel.send(":muscle::eyes::muscle:");
-  }
-});
+if (message.content.startsWith("fight me bitch")) {
+            message.channel.send(":muscle::eyes::muscle:");
+          }
 
-client.on("message", (message) => {
 if (message.content.startsWith ('asl')) {
     message.channel.send("241/Country/USA");
   }
-});
 
-client.on("message", (message)=>{
-    if (message.channel.type == 'dm' || message.channel.type == 'group') {
-        var embed = new Discord.RichEmbed()
-        .setAuthor(message.author.username, message.author.avatarURL)
-        .setDescription(message.content)
-        .setTimestamp(new Date())
-        .setColor('#C735D4');
-        client.channels.get("276351839312805888").send(embed);
-    }
-});
-
-client.on("message", (message) => {
 if (message.content.includes ('╯°□°）╯︵ ┻━┻')) {
     message.channel.send("┬─┬ ノ( ゜-゜ノ)");
   }
-});
 
-client.on("message", (message) => {
-if (message.content.includes ('vore')) {
-    client.users.get("264945356884672524").sendMessage("***RAT JAIL***");
-  }
-});
-
-client.on("message", (message) => {
 if (message.content.includes ('sad')) {
     message.channel.send("i love you!");
   }
-});
 
-client.on("message", (message) => {
-  if (message.content.startsWith("*shrugs*")) {
+if (message.content.startsWith("*shrugs*")) {
     message.channel.send(" ¯\\_(ツ)_/¯ ");
   }
-});
 
+if (message.content.includes ('good boi') && message.content.includes ('al')) {
+    message.channel.send("( ͡° ᴥ ͡°)");
+  }
 
+if (message.content.includes ('good boi') && message.content.includes ('alfie')) {
+    message.channel.send("( ͡° ᴥ ͡°)");
+  }
 
+if (message.content.includes ('good boi') && message.content.includes ('alfred')) {
+    message.channel.send("(◡‿◡✿)");
+  }
 
-
-client.on("message", (message) => {
 if (message.content.includes ('depressed')) {
     message.channel.send("i love you!");
   }
-});
 
-client.on("message", (message) => {
 if (message.author.id !== '264945356884672524' && message.content.includes ('joe')) {
     message.channel.send(":eyes:");
   }
 });
 
-client.on("message", (message) => {
-if (message.author.id !== '264945356884672524' && message.content.includes ('oliver')) {
-    client.users.get("264945356884672524").sendMessage("oli kirklunn mention");
-  }
-});
-
+//heroku stay online thing
 var http = require("http");
 setInterval(function() {
     http.get("http://hetaliabot2.herokuapp.com");
 }, 900000);
 
 
-client.on("message", (message) => {
-if (message.author.id !== '264945356884672524' && message.content.includes ('2p america')) {
-    client.users.get("264945356884672524").sendMessage("al mention");
-  }
-});
-
-client.on("message", (message) => {
-if (message.author.id !== '264945356884672524' && message.content.includes ('2p england')) {
-    client.users.get("264945356884672524").sendMessage("oli kirklun mention");
-  }
-});
-
-client.on("message", (message) => {
-if (message.author.id !== '264945356884672524' && message.content.includes ('hitler')) {
-    client.users.get("264945356884672524").sendMessage("moustache man mention");
-  }
-});
-
-client.on("message", (message) => {
-if (message.author.id !== '264945356884672524' && message.content.includes ('nazi')) {
-    client.users.get("264945356884672524").sendMessage("idea mention");
-  }
-});
-
-
+// DMS TO ME
 client.on("message", (message) => {
 if (message.author.id !== '264945356884672524' && message.content.includes ('allan')) {
     client.users.get("264945356884672524").sendMessage("hoseph mention");
   }
+
+if (message.author.id !== '264945356884672524' && message.content.includes ('oliver')) {
+      client.users.get("264945356884672524").sendMessage("oli kirklunn mention");
+    }
+
+if (message.content.includes ('vore')) {
+        client.users.get("264945356884672524").sendMessage("***RAT JAIL***");
+      }
+
+if (message.author.id !== '264945356884672524' && message.content.includes ('2p america')) {
+      client.users.get("264945356884672524").sendMessage("al mention");
+    }
+
+if (message.author.id !== '264945356884672524' && message.content.includes ('2p england')) {
+      client.users.get("264945356884672524").sendMessage("oli kirklun mention");
+    }
+
+if (message.content.includes ('veggie burgers')) {
+      client.users.get("264945356884672524").sendMessage("vb mention");
+    }
+
+if (message.author.id !== '264945356884672524' && message.content.includes ('nazi')) {
+        client.users.get("264945356884672524").sendMessage("idea mention");
+      }
+if (message.author.id !== '264945356884672524' && message.content.includes ('hitler')) {
+          client.users.get("264945356884672524").sendMessage("moustache man mention");
+        }
+
 });
 
-
+// DMS to OTHERS
 
 client.on("message", (message) => {
 if (message.author.id !== '317540893282271234' && message.content.includes ('luxembourg')) {
@@ -213,32 +181,7 @@ if (message.author.id !== '317540893282271234' && message.content.includes ('lux
   }
 });
 
-
-client.on("message", (message) => {
-if (message.content.includes ('good boi') && message.content.includes ('al')) {
-    message.channel.send("( ͡° ᴥ ͡°)");
-  }
-});
-
-client.on("message", (message) => {
-if (message.content.includes ('good boi') && message.content.includes ('alfie')) {
-    message.channel.send("( ͡° ᴥ ͡°)");
-  }
-});
-
-client.on("message", (message) => {
-if (message.content.includes ('good boi') && message.content.includes ('alfred')) {
-    message.channel.send("(◡‿◡✿)");
-  }
-});
-
-client.on("message", (message) => {
-if (message.content.includes ('veggie burgers')) {
-    client.users.get("264945356884672524").sendMessage("vb mention");
-  }
-});
-
-
+//STATE REACTIONS
 
 client.on("message", (message) => {
 if (message.content.toLowerCase().includes ('texas')) {
@@ -247,69 +190,48 @@ if (message.content.toLowerCase().includes ('texas')) {
 });
 
 client.on("message", (message) => {
-if (message.content.toLowerCase().includes ('georgia')) {
-    message.channel.send(":smiling_imp: :violin:");
-  }
-});
-
-client.on("message", (message) => {
 if (message.content.toLowerCase().includes ('new jersey')) {
     message.channel.send(":blueberries:");
   }
+
+if (message.content.toLowerCase().includes ('texas')) {
+      message.channel.send(":cowboy:");
+    }
+
+  if (message.content.toLowerCase().includes ('georgia')) {
+      message.channel.send(":smiling_imp: :violin:");
+    }
 });
 
-client.on("message", (message) => {
-if (message.author.id === '258431184785113094' && message.content.includes ('sad')) {
-    message.channel.send("I LOVE YOU SO MUCH AL AND I BELIEVE IN YOU!!!");
-  }
-});
-
-client.on("message", (message) => {
-if (message.author.id === '258431184785113094' && message.content.includes ('fail')) {
-    message.channel.send("I LOVE YOU SO MUCH AL,  I BELIEVE IN YOU!!! <3");
-  }
-});
-
-client.on("message", (message) => {
-if (message.author.id === '258431184785113094' && message.content.includes ('cant')) {
-    message.channel.send("I LOVE YOU SO MUCH AL AND I BELIEVE IN YOU!!!");
-  }
-});
-
+// MATH REACTIONS
 
 client.on('message', message =>{
-	if (message.content.includes ('rock paper scissors')) {
-		var choices = ['rock','paper','scissors'];
-		var response = choices[Math.floor(Math.random()*3)];
-		setTimeout(function(){
-    message.channel.send(' :heart: I choose '+response);}, 10000);
-	}
-});
-
-client.on('message', message =>{
-	if (message.content.includes ('roll die')) {
-		var choices = ['1','2','3','4','5','6'];
-		var response = choices[Math.floor(Math.random()*6)];
+    if (message.content.includes ('roll die')) {
+        var choices = ['1','2','3','4','5','6'];
+        var response = choices[Math.floor(Math.random()*6)];
     message.channel.send(' :sweat_drops:'+response);
-	}
-});
+    }
 
-
-
-client.on('message', message =>{
-	if (message.content.includes ('dnd20')) {
-		var choices = ['17','16','15','20','18','19','14','13','12','11','10','9','8','7','6','5','4','3','2','1'];
-		var response = choices[Math.floor(Math.random()*20)];
+if (message.content.includes ('dnd20')) {
+        var choices = ['17','16','15','20','18','19','14','13','12','11','10','9','8','7','6','5','4','3','2','1'];
+        var response = choices[Math.floor(Math.random()*20)];
     message.channel.send(' :sweat_drops:'+response);
-	}
+    }
+
+if (message.content.includes ('rock paper scissors')) {
+      var choices = ['rock','paper','scissors'];
+      var response = choices[Math.floor(Math.random()*3)];
+      setTimeout(function(){
+      message.channel.send(' :heart: I choose '+response);}, 10000);
+    }
+
+if (message.content.toLowerCase().includes ('romano bot') || message.content.toLowerCase().includes ('romanobot') || message.content.toLowerCase().includes ('romabot') || message.content.toLowerCase().includes ('roma bot')) {
+  		var choices = ['yeah.. i owe him .3 BTC but i spent .1 BTC on mcdonalds last week so im a bit short... :thinking:','I sent him an IOU but I think he just deleted it..','I tried to pay him back in coupons to olive garden and he sent me malware','I know him!','i told him about pineapple pizza once... he gave me a glitch after that','Pizza, pasta, put it in a box-'];
+  		var response = Math.floor(Math.random() * choices.length);
+      message.channel.send(choices[response]);
+  	}
 });
 
-client.on("message", (message => {
-    if (!message.content.startsWith('dm') || message.author.bot) return;
-    const args = message.content.split(' ');
-    const taggedUser = message.mentions.users.first();
-    taggedUser.send(args.splice(2).join(" "));
-}));
 	  
 	
 	
